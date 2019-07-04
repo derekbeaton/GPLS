edc24e8ff4caa666163de4b479c47845772de1c4
Derek <derekbeaton@users.noreply.github.com>
2019-07-04 10:52:04 -0400

    I think I've locked this down.

--- reframe/gplssvd_tests.R
+++ reframe/gplssvd_tests.R
@@ -3,7 +3,7 @@
 library(TExPosition)
 library(ours)
 library(GSVD)
-
+library(rrr)
 
 ### can we get back PLSC, CCA, and RRR from gplssvd? If so we're in some very serious business.
 
@@ -45,6 +45,13 @@ cca.res <- gsvd(
   RW=crossprod(Y)
 )
 
+
+cca.res <- gsvd(
+  DAT=R,
+  LW=crossprod(X) %^% (-1),
+  RW=crossprod(Y) %^% (-1)
+)
+
 cca.res$maybe_lx <- (X %*% cca.res$u)
 cca.res$maybe_ly <- (Y %*% cca.res$v)
 
@@ -57,21 +64,65 @@ gplssvd_cca <- GSVD:::gplssvd(
   X, Y,
   # XLW=(tcrossprod(X)), 
   # YLW=(tcrossprod(Y)),
+  XRW=crossprod(X) %^% (-1),
+  YRW=crossprod(Y) %^% (-1)
+)
+
+
+### this one is the answer.
+gplssvd_cca <- GSVD:::gplssvd(
+X = X %*% (crossprod(X) %^% (-1)),
+Y = (Y %*% (crossprod(Y) %^% (-1))),
+XRW=crossprod(X),
+YRW=crossprod(Y)
+)
+
+gplssvd_cca2 <- GSVD:::gplssvd(
+  X = X %^% (-1),
+  Y = Y %^% (-1),
   XRW=crossprod(X),
   YRW=crossprod(Y)
 )
 
+  ### figure out all the optimizations here
+
 
 cc.res <- cancor(X,Y)
 
-(crossprod(X) %^% (1/2)) %*% (crossprod(X) %^% -1) %*% t(X) %*% Y %*% (crossprod(Y) %^% -1) %*% (crossprod(Y) %^% (1/2))
+svd.res0 <- svd((crossprod(X) %^% (1/2)) %*% (crossprod(X) %^% -1) %*% t(X) %*% Y %*% (crossprod(Y) %^% -1) %*% (crossprod(Y) %^% (1/2)))
 
+svd.res <- svd((crossprod(X) %^% (-1/2)) %*% t(X) %*% Y %*% (crossprod(Y) %^% (-1/2)))
+
+
+svd((crossprod(X) %^% (-1/2)) %*% t(X) %*% Y %*% (crossprod(Y) %^% (-1/2)))
+
+(crossprod(X) %^% (1/2))
+t(X %^% (1/2)) %*% (X %^% (1/2))
 
 
 #### this needs to be solved:
 (crossprod(X) %^% (1/2)) %*% (crossprod(X) %^% -1)
 
 
+(crossprod(X) %^% (-1/2))
+
+
+t(X %^% (1/2)) %*% (X %^% (-1))
+
+
+
+### a good reference see rrr.nonmiss: http://ftp.uni-bayreuth.de/math/statlib/S/rrr.s
+
+Sxy<-t(X)%*%Y
+#Sxmin12<-sym.matr.power(t(X)%*%X,-0.5)
+Sxmin12<-(crossprod(X) %^% (-1/2))
+D<-Sxmin12%*%Sxy
+svdobj<-svd(D)
+U<-as.matrix(svdobj$u)
+V<-as.matrix(svdobj$v)
+L<-svdobj$d
+Beta<-t(t(Sxmin12%*%U)*L)
+Alpha<-V
 
 rrr.res <- gsvd(
   DAT=(crossprod(X) %^% -1) %*% R,
@@ -83,7 +134,21 @@ rrr.res$ly <- (Y %*% rrr.res$q)
 (X %*% rrr.res$u)
 (Y %*% rrr.res$v)
 
+### this seems to be the answer
+gplssvd_rrr <- GSVD:::gplssvd(
+  X = X %*% (crossprod(X) %^% (-1)),
+  Y = Y,
+  XRW=crossprod(X)
+)
 
+gplssvd_rrr2 <- GSVD:::gplssvd(
+  X = X %^% (-1),
+  Y = Y,
+  XRW=crossprod(X)
+)
+
+
+rrr_res <- rrpack::rrr(Y,X)
 
 
 
