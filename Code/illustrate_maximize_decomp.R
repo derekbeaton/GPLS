f682d371a49cb695d8f3f8a72d145819fd232a89
Derek <derekbeaton@users.noreply.github.com>
2019-09-17 23:11:48 -0400

    updates and corrections. to pick up at the algorithm & maximization to harmonize with new notation style/GPPLSVD

---
+++ Code/illustrate_maximize_decomp.R
@@ -0,0 +1,62 @@
+library(GPLS)
+data("snps.druguse")
+
+X <- make_data_disjunctive(snps.druguse$DATA1)
+Y <- make_data_disjunctive(snps.druguse$DATA2)
+
+ca_preproc_X <- ca_preproc(X)
+  M_X <- diag(1/ca_preproc_X$m)
+  W_X <- diag(1/ca_preproc_X$w)
+  ZX_tilde <- (M_X %^% (1/2)) %*% ca_preproc_X$Z %*% (W_X %^% (1/2))  
+
+ca_preproc_Y <- ca_preproc(Y)
+  M_Y <- diag(1/ca_preproc_Y$m)
+  W_Y <- diag(1/ca_preproc_Y$w)
+  ZY_tilde <- (M_Y %^% (1/2)) %*% ca_preproc_Y$Z %*% (W_Y %^% (1/2))  
+
+ZR <- t(ZX_tilde) %*% ZY_tilde  
+plsca_reg_results <- plsca_reg(X, Y)
+
+
+
+
+## illustration of maximization (Eq. 9)
+
+t(plsca_reg_results$lx) %*% plsca_reg_results$ly
+diag(t(plsca_reg_results$lx) %*% plsca_reg_results$ly)
+plsca_reg_results$d
+
+
+t((M_X %^% (1/2)) %*% ca_preproc_X$Z %*% W_X %*% plsca_reg_results$p[,1]) %*%
+  ((M_Y %^% (1/2)) %*% ca_preproc_Y$Z %*% W_Y %*% plsca_reg_results$q[,1])
+
+t(ZX_tilde %*% plsca_reg_results$u[,1]) %*% (ZY_tilde %*% plsca_reg_results$v[,1])
+
+t(plsca_reg_results$u[,1]) %*% t(ZX_tilde) %*% ZY_tilde %*% plsca_reg_results$v[,1]
+
+t(plsca_reg_results$u[,1]) %*% ZR %*% plsca_reg_results$v[,1]
+
+
+
+
+t(plsca_reg_results$p) %*% W_X %*% plsca_reg_results$p
+t(plsca_reg_results$q) %*% W_Y %*% plsca_reg_results$q
+
+
+
+## illustration of double decomposition (Eq. 10)
+
+(plsca_reg_results$tx %*% t(plsca_reg_results$u_hat)) / ZX_tilde
+
+#
+# (YLW %^% (-1/2)) %*% (tx %*% diag(betas) %*% t(v)) %*% (YRW %^% (-1/2))
+(plsca_reg_results$tx %*% diag(plsca_reg_results$betas) %*% t(plsca_reg_results$v)) / plsca_reg_results$Y_hat
+## and re-express it as
+
+## illustration of OLS (Eq. 11)
+
+ZX_tilde %*%  (( t(ZX_tilde) %*% ZX_tilde ) %^% (-1)) %*% t(ZX_tilde) %*% ca_preproc_Y$Z
+
+ca_preproc_X$Z %*%  (( t(ca_preproc_X$Z) %*% ca_preproc_X$Z ) %^% (-1)) %*% t(ca_preproc_X$Z) %*% ca_preproc_X$Z
+
+(ZX_tilde %*%  (( t(ZX_tilde) %*% ZX_tilde ) %^% (-1)) %*% t(ZX_tilde) %*% ca_preproc_Y$Z) / plsca_reg_results$Y_reconstructed
