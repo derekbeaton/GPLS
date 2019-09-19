7432109908b4b27a4e9a6f95a9e617aff8eaf2e4
Derek <derekbeaton@users.noreply.github.com>
2019-09-19 11:07:12 -0400

    still making the new notation work from Eq. 9 onwards, and making a companion script to show the relationships.

--- Code/illustrate_maximize_decomp.R
+++ Code/illustrate_maximize_decomp.R
@@ -1,3 +1,4 @@
+library(GSVD)
 library(GPLS)
 data("snps.druguse")
 
@@ -5,14 +6,14 @@ X <- make_data_disjunctive(snps.druguse$DATA1)
 Y <- make_data_disjunctive(snps.druguse$DATA2)
 
 ca_preproc_X <- ca_preproc(X)
-  M_X <- diag(1/ca_preproc_X$m)
-  W_X <- diag(1/ca_preproc_X$w)
-  ZX_tilde <- (M_X %^% (1/2)) %*% ca_preproc_X$Z %*% (W_X %^% (1/2))  
+  M_X <- diag(ca_preproc_X$m)
+  W_X <- diag(ca_preproc_X$w)
+  ZX_tilde <- (M_X %^% (-1/2)) %*% ca_preproc_X$Z %*% (W_X %^% (-1/2))  
 
 ca_preproc_Y <- ca_preproc(Y)
-  M_Y <- diag(1/ca_preproc_Y$m)
-  W_Y <- diag(1/ca_preproc_Y$w)
-  ZY_tilde <- (M_Y %^% (1/2)) %*% ca_preproc_Y$Z %*% (W_Y %^% (1/2))  
+  M_Y <- diag(ca_preproc_Y$m)
+  W_Y <- diag(ca_preproc_Y$w)
+  ZY_tilde <- (M_Y %^% (-1/2)) %*% ca_preproc_Y$Z %*% (W_Y %^% (-1/2))  
 
 ZR <- t(ZX_tilde) %*% ZY_tilde  
 plsca_reg_results <- plsca_reg(X, Y)
@@ -26,9 +27,15 @@ t(plsca_reg_results$lx) %*% plsca_reg_results$ly
 diag(t(plsca_reg_results$lx) %*% plsca_reg_results$ly)
 plsca_reg_results$d
 
+# t((M_X %^% (-1/2)) %*% ca_preproc_X$Z %*% (W_X %^% (-1)) %*% plsca_reg_results$p[,1]) %*%
+#   ((M_Y %^% (-1/2)) %*% ca_preproc_Y$Z %*% (W_Y %^% (-1)) %*% plsca_reg_results$q[,1])
 
-t((M_X %^% (1/2)) %*% ca_preproc_X$Z %*% W_X %*% plsca_reg_results$p[,1]) %*%
-  ((M_Y %^% (1/2)) %*% ca_preproc_Y$Z %*% W_Y %*% plsca_reg_results$q[,1])
+t((M_X %^% (-1/2)) %*% ca_preproc_X$Z %*% (W_X %^% (-1)) %*%  plsca_reg_results$p[,1]  ) %*%
+  ((M_Y %^% (-1/2)) %*% ca_preproc_Y$Z %*% (W_Y %^% (-1)) %*%  plsca_reg_results$q[,1]  )
+
+
+t((M_X %^% (-1/2)) %*% ca_preproc_X$Z %*% (W_X %^% (-1/2)) %*% (W_X %^% (1/2)) %*%  (W_X %^% (-1/2) %*% plsca_reg_results$u[,1])  ) %*%
+  ((M_Y %^% (-1/2)) %*% ca_preproc_Y$Z %*% (W_Y %^% (-1/2))%*% (W_Y %^% (1/2)) %*%  (W_Y %^% (-1/2) %*% plsca_reg_results$v[,1])  )
 
 t(ZX_tilde %*% plsca_reg_results$u[,1]) %*% (ZY_tilde %*% plsca_reg_results$v[,1])
 
@@ -36,10 +43,12 @@ t(plsca_reg_results$u[,1]) %*% t(ZX_tilde) %*% ZY_tilde %*% plsca_reg_results$v[
 
 t(plsca_reg_results$u[,1]) %*% ZR %*% plsca_reg_results$v[,1]
 
-
-
-
+## diagonal identity, and orthogonal.
 t(plsca_reg_results$p) %*% W_X %*% plsca_reg_results$p
+t(plsca_reg_results$tx) %*% plsca_reg_results$tx
+t(plsca_reg_results$lx) %*% plsca_reg_results$lx
+
+## diagonal identity, but not orthogonal.
 t(plsca_reg_results$q) %*% W_Y %*% plsca_reg_results$q
 
 
@@ -50,8 +59,12 @@ t(plsca_reg_results$q) %*% W_Y %*% plsca_reg_results$q
 
 #
 # (YLW %^% (-1/2)) %*% (tx %*% diag(betas) %*% t(v)) %*% (YRW %^% (-1/2))
-(plsca_reg_results$tx %*% diag(plsca_reg_results$betas) %*% t(plsca_reg_results$v)) / plsca_reg_results$Y_hat
+
+((M_X %^% (-1)) %^% (-1/2)) %*% (plsca_reg_results$tx %*% diag(plsca_reg_results$betas) %*% t(plsca_reg_results$v)) %*% ((W_X %^% (-1)) %^% (-1/2)) / plsca_reg_results$Y_reconstructed
 ## and re-express it as
+
+
+
 
 ## illustration of OLS (Eq. 11)
 
